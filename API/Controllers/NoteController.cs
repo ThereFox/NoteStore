8fd6ee8ecbabe5317271e57bfaf3100a2115a17b
ThereFox <ThereFox@tf.tf>
2024-12-07 12:27:17 +0400

    update api

--- API/Controllers/NoteController.cs
+++ API/Controllers/NoteController.cs
@@ -1,6 +1,57 @@
+using Application.Commands;
+using Application.Commands.Abstraction;
+using Application.Requests;
+using Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+using NoteStore.Domain.ValueObjects;
+
 namespace API.Controllers;
 
-public class NoteController
+[Controller]
+[Route("/notes")]
+public class NoteController : Controller
 {
+    private NoteQueryService _queryService;
+    private ICommandHandler<CreateNoteCommand> _createNoteCommandHandler;
+
+    public NoteController(NoteQueryService queryService, ICommandHandler<CreateNoteCommand> createNoteCommandHandler)
+    {
+        _queryService = queryService;
+        _createNoteCommandHandler = createNoteCommandHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody]CreateNoteRequest request)
+    {
+        var validateType = NoteGroup.Create(request.noteGroup);
+
+        if (validateType.IsFailure)
+        {
+            return BadRequest("Invalid note group");
+        }
+            
+        var command = new CreateNoteCommand()
+        {
+            group = validateType.Value,
+            Content = request.content,
+            Descriprion = request.description,
+            Header = request.title
+        };
+        
+        var handlerResult = await _createNoteCommandHandler.Handle(command);
+
+        return Ok(handlerResult.IsSuccess);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var note = await _queryService.GetById(id);
+        if (note.IsFailure)
+        {
+            return NotFound(note.Error);
+        }
+        return Ok(note.Value);
+    }
     
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,22 @@
+using Application;
+using Persistense.Common.DI;
+using Persistense.Serching.Elastick;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services
+    .AddControllers();
+
+builder
+    .Services
+    .AddApplicationServices()
+    .AddCassandra("localhost", 9042)
+    .AddNoteStores()
+    .AddElastickSearch("http://127.0.0.1:9200", "default_index_for_app");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
